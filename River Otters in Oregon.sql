837dd4b7acbcc9cfd700fff9f7293110f7f1d7a3
Bear B. <illustratorbear@gmail.com>
2025-10-14 10:34:39 -0700

    Update River Otters in Oregon.sql
    added title and 3 questions

--- River Otters in Oregon.sql
+++ River Otters in Oregon.sql
@@ -1,1 +1,5 @@
+In this SQL project I will answer the following questions from a database gathered with iNaturalist verified observations of river otters in Oregon.
 
+1. What general areas in Oregon have the most sighting
+2. What time of year are the otters most likely to be observed?
+3. What year had the highest amount of 
