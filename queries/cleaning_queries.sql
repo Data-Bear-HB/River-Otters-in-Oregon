8cc3ca325b702185081f85e50dcc4f7bc1c75616
Bear B. <illustratorbear@gmail.com>
2025-10-15 15:41:42 -0700

    Add location and season/year columns to otter_data
    Added columns for general location and season/year in otter_data table. 
    Updated season_year based on observed_on data 
     included a query to count sightings by season and year

--- queries/cleaning_queries.sql
+++ queries/cleaning_queries.sql
@@ -1,25 +1,26 @@
 # The goal of cleaning data in this project is to create a dashboard that answers the questions of otter sightings:
+  
 -by year (2014-2024)
 -by season (Spring, Summer, Fall, Winter)
 -by place (group by area hubs, county, city, coastal area, etc)
 
-# 1. Make a query accessible column for location: 
+# Make a query accessible column for location: 
   
   ALTER TABLE otter_data
 ADD COLUMN general_location VARCHAR(255);
 
-# 2. Make a query accessible column that includes season and year:
+# Make a query accessible column that includes season and year:
 
 ALTER TABLE otter_data
 ADD COLUMN season_year VARCHAR(50)
 
-#Convert observed_on column data to "Season, Year" data in the season_year column
+# Convert observed_on column data to "Season, Year" data in the season_year column
 
 UPDATE otter_data
 SET season_year = 
     CASE 
         WHEN EXTRACT(MONTH FROM observed_on) IN (12, 1, 2) THEN 
-            'Winter ' EXTRACT(YEAR FROM observed_on)
+            'Winter ' || EXTRACT(YEAR FROM observed_on)
         WHEN EXTRACT(MONTH FROM observed_on) IN (3, 4, 5) THEN 
             'Spring ' || EXTRACT(YEAR FROM observed_on)
         WHEN EXTRACT(MONTH FROM observed_on) IN (6, 7, 8) THEN 
@@ -27,4 +28,12 @@ SET season_year =
         WHEN EXTRACT(MONTH FROM observed_on) IN (9, 10, 11) THEN 
             'Fall ' || EXTRACT(YEAR FROM observed_on)
     END;
+
+# Initial findings for "What season has the most sightings?"
+
+SELECT season_year, COUNT(*)
+FROM otter_data
+GROUP BY season_year
+ORDER BY COUNT DESC;
+
 
